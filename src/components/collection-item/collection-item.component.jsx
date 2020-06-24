41d0142e39d2973dba2bc503c5db3698ff91287d
Dro <lorddro1532@gmail.com>
2020-06-24 17:10:11 +0100

    Implemented Reducers for CartItems and Updated shop data

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,17 +1,35 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import { addItem } from './../../redux/cart/cart.actions';
+
+import CustomButton from './../custom-button/custom-button.component';
 
 import './collection-item.styles.scss';
 
-const CollectionItem = ({name, imageUrl, price}) => (
-  <div className='collection-item'>
+const CollectionItem = ({item, addItem}) => {
+  const { name, price, imageUrl } = item;
+  return (
+    <div className='collection-item'>
     <div style={{
       backgroundImage: `url(${imageUrl})`
     }} className='image'></div>
     <div className='collection-footer'>
       <span className='name'>{name}</span>
-      <span className='price'>{price}</span>
+      <span className='price'>${price}</span>
     </div>
+    <CustomButton onClick={()=>addItem(item)} inverted>ADD TO CART </CustomButton>
   </div>
-)
+  );
+}
+  
 
-export default CollectionItem;
+
+const mapDispatchToProps = dispatch => ({
+  addItem: item => dispatch(addItem(item))
+})
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(CollectionItem);
